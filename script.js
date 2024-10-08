8ff240ac88b68e86a37491bf031800052f5c7929
Vikas <vickydelhi03@gmail.com>
2024-10-08 18:30:01 +0530

    strikethrough added and some css changes.

--- script.js
+++ script.js
@@ -2,23 +2,38 @@ let inputs = document.getElementById('input-box');
 let text = document.querySelector('.text');
 let iselector;
 
-function Add(){
-    if(inputs.value == ''){
-        alert('Please Enter Task')
-    }
-    else{      
+function Add() {
+    if (inputs.value === '') {
+        alert('Please Enter Task');
+    } else {
+       
         let newelement = document.createElement('ul');
-        newelement.innerHTML=`${inputs.value}<i class="fa-solid fa-xmark"></i>`;
+      
+        newelement.innerHTML = `<span class="task-text">${inputs.value}</span><i class="fa-solid fa-xmark"></i>`;
+                
         text.appendChild(newelement);
-        inputs.value='';
+        inputs.value = '';
 
-            
+       
         iselector = newelement.querySelector('i');
         iselector.addEventListener('click', remove);
-        function remove(){
-            newelement.remove()          
-        }      
+
+        
+        let taskText = newelement.querySelector('.task-text');
+        taskText.addEventListener('click', toggleStrikethrough);
+
+        function remove() {
+            newelement.remove();
+        }
+
+        function toggleStrikethrough() {
+            taskText.classList.toggle('completed'); 
+        }
     }
-} 
+}
 
-
+inputs.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        Add();
+    }
+});
